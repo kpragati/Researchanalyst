1b1e365496020ceeb22fa90e5d11fe072473844d
kpragati <kalpandepragati@gmail.com>
2025-05-19 22:53:47 +0530

    create data function

--- ResearchAnalyst.py
+++ ResearchAnalyst.py
@@ -1,13 +1,19 @@
-from openai import OpenAI
+import openai 
+import os
 import pandas as pd
 
-OpenAI.api_key = "API_KEY"
+openai.api_key = os.environ.get("OPENAI_API_KEY") #Using direct openai module
+df = pd.read_csv("/Users/pragati-devhub/Downloads/machine-readable-business-employment-data-dec-2024-quarter.csv") 
 
-df = "/Users/pragati-devhub/Downloads/machine-readable-business-employment-data-dec-2024-quarter.csv"
 
-
-def anlyseDate():
-    response = OpenAI.chat.completion.create(
-          
-
+def anlyseData(df):
+    response = openai.Completion.create(
+          model = "gpt-4o",
+          message = [{"role": "user", "content":f"You are research assistent. Provide key insight from {df} in point form"}],
+          temperature = 0.2,
+          max_tokens = 300
     )
+
+    return response.choices[0].message.content
+
+print(anlyseData(df))
